6f722bd42fa3d062614a78c3d96528c6f6aa1285
Mahboob <mahboob.eid@googlemail.com>
2017-08-15 19:24:22 +0530

    ApiAllDetails.cs was added to get the model to hold all details

---
+++ MVC.Models/MVC.Models/ApiAllDetails.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace MVC.Models
+{
+
+    using System.Collections.Generic;
+
+    public class ApiAllDetails
+    {
+        public string Workbook { get; set; }
+        public Dictionary<string,string> WbPreferencesAndApiCalls  { get; set; }
+        
+    }
+
+
+
+    public class ApiAllDetailsResponse : ApiResponseMessage<ApiAllDetails>
+    {
+
+        public ApiAllDetailsResponse()
+        {
+            this.InitializeContent();
+        }
+
+
+        public override void InitializeContent()
+        {
+            base.Content = new List<ApiAllDetails>();
+        }
+    }
+}
--- MVC.Models/MVC.Models/WbInfoBasedOnApiCallResponse.cs
+++ MVC.Models/MVC.Models/WbInfoBasedOnApiCallResponse.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿
 
 namespace MVC.Models
 {
+
+    using System.Collections.Generic;
+
     public class WbInfoBasedOnApiCall
     {
         public string Workbook { get; set; }
